5b5aff7251f2d0fe3b5a667d52110cc1e09d9c95
Harshit-Rawat-0950 <harshitdep@gmail.com>
2026-03-23 13:41:34 +0530

    Junit added

---
+++ Project Folder/src/EventManagementSubSystem/EventManagerTest.java
@@ -0,0 +1,14 @@
+package EventManagementSubSystem;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class EventManagerTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
