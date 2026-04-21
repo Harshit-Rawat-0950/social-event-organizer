a87a88d0d447d0fe105f2c0f5161f9e0d4421b4d
Harshit-Rawat-0950 <harshitdep@gmail.com>
2026-04-21 11:48:39 +0530

    Commit
    Commit kara mene

--- Project Folder/src/UserMangerSubSystem/PassWordManager.java
+++ Project Folder/src/UserMangerSubSystem/PassWordManager.java
@@ -1,7 +1,33 @@
 package UserMangerSubSystem;
-
+import javax.crypto.Cipher;
+import javax.crypto.spec.SecretKeySpec;
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
 public class PassWordManager {
 
+	private static final String ALGORITHM = "AES";
+
+    // Must be 16 characters for AES-128
+    private static final String SECRET_KEY = "MySecretKey12345";
+
+    private static SecretKeySpec getKey() {
+        return new SecretKeySpec(SECRET_KEY.getBytes(StandardCharsets.UTF_8), ALGORITHM);
+    }
+
+    public static String encrypt(String plainText) throws Exception {
+        Cipher cipher = Cipher.getInstance(ALGORITHM);
+        cipher.init(Cipher.ENCRYPT_MODE, getKey());
+        byte[] encryptedBytes = cipher.doFinal(plainText.getBytes(StandardCharsets.UTF_8));
+        return Base64.getEncoder().encodeToString(encryptedBytes);
+    }
+
+    public static String decrypt(String encryptedText) throws Exception {
+        Cipher cipher = Cipher.getInstance(ALGORITHM);
+        cipher.init(Cipher.DECRYPT_MODE, getKey());
+        byte[] decodedBytes = Base64.getDecoder().decode(encryptedText);
+        byte[] decryptedBytes = cipher.doFinal(decodedBytes);
+        return new String(decryptedBytes, StandardCharsets.UTF_8);
+    }
 	public String getPassWord(int id) {
 		// TODO Auto-generated method stub
 		return null;
--- Project Folder/src/UserMangerSubSystem/UserRegistration.java
+++ Project Folder/src/UserMangerSubSystem/UserRegistration.java
@@ -1,10 +1,20 @@
 package UserMangerSubSystem;
 
+import java.io.IOException;
+import java.util.Scanner;
+
+import ReportDashboardSubSystem.DashboardFacade;
+
 public class UserRegistration {
 
-	public void showRegistration(UserRepository userRepository) {
-		// TODO Auto-generated method stub
-		
+	public void showRegistration(UserRepository Repo) {
+		DashboardFacade.displayLoginPage();//if needed
+		Scanner sc = new Scanner(System.in);
+		System.out.println("===== User Registration =====");
+        System.out.print("Enter id: ");
+        int id = sc.nextInt();
+        PassWordManager pm = new PassWordManager();
+        System.out.print("Enter password :");
 	}
 
 }
